0537cef7ee5a84846aff47e8f5edf5c86a041832
Danieltm95 <docampoc95@gmail.com>
2023-05-18 18:03:17 -0500

    Se agrego al controlador getServices una funcion que le autollena la base de datos a cualqueira que lo use! con ejemplos cotidianos

--- src/controllers/getServices.js
+++ src/controllers/getServices.js
@@ -1,6 +1,93 @@
 const { Service, User } = require('../db.js');
+const {createService} = require('./createService.js');
 
 const getServices = async () => {
+
+    const servicesForTesting = [
+        {
+          "nameService": "Corte de pelo",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Armenia"
+          },
+          "imageUrl": ["https://www.mascotas.com/wp-content/uploads/2020/01/corte-de-pelo-mascotas.jpg"],
+          "description": "Corte de pelo para perros y gatos",
+          "pricePerHour": 10,
+          "typeService": "Peluqueria"
+        },
+        {
+          "nameService": "Remodelación de cocina",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Bogotá"
+          },
+          "imageUrl": ["https://example.com/remodelacion-cocina.jpg"],
+          "description": "Servicio de remodelación de cocinas",
+          "pricePerHour": 50,
+          "typeService": "Construccion"
+        },
+        {
+          "nameService": "Reparación de tuberías",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Medellín"
+          },
+          "imageUrl": ["https://example.com/reparacion-tuberias.jpg"],
+          "description": "Reparación y mantenimiento de tuberías",
+          "pricePerHour": 30,
+          "typeService": "Plomeria"
+        },
+        {
+          "nameService": "Pintura de interiores",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Cali"
+          },
+          "imageUrl": ["https://example.com/pintura-interiores.jpg"],
+          "description": "Servicio de pintura de interiores",
+          "pricePerHour": 40,
+          "typeService": "Pintura"
+        },
+        {
+          "nameService": "Diseño estructural",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Barranquilla"
+          },
+          "imageUrl": ["https://example.com/diseno-estructural.jpg"],
+          "description": "Diseño estructural de edificaciones",
+          "pricePerHour": 80,
+          "typeService": "Ingenieria"
+        },
+        {
+          "nameService": "Diseño arquitectónico",
+          "location": {
+            "pais": "Colombia",
+            "ciudad": "Cartagena"
+          },
+          "imageUrl": ["https://example.com/diseno-arquitectonico.jpg"],
+          "description": "Servicio de diseño arquitectónico",
+          "pricePerHour": 70,
+          "typeService": "Arquitectura"
+        }
+      ];
+      //ESTO ES PARA QUE SE LES LLENE LA BASE DE DATOS APRA QUE PUEDAN USARLOS SIEMPRE SIN TENER QUE HACER MNUCHOS POST ! A LOS DEL FRONT
+        const dbFull = await Service.findAll({
+            include: [{
+                model: User,
+                attributes: ['name', 'email'],
+                through: {
+                    attributes: [],
+                }
+            }]
+        });
+        if(dbFull.length > 0) {
+
+        } else {
+        servicesForTesting.forEach( async (service) => {
+          await createService(service);
+      }) }
+
 
     const dbServices = await Service.findAll({
         include: [{
