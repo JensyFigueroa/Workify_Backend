db288931523ed8f7bb17d2a9ef4fc80206dbd02e
DAMC <david.moreno.cardenas@gmail.com>
2023-06-01 21:18:28 -0400

    nuevo payment mejorado

--- src/controllers/newPayment.js
+++ src/controllers/newPayment.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Stripe = require('stripe')
+const { Service, User } = require('../db.js');
 // const { Service, User } = require('../db.js');
 
 const {KEY_STRIPE}=process.env;
@@ -8,7 +9,7 @@ const stripe = new Stripe(KEY_STRIPE);
 
 const newPayment = async ( cartsItems) =>{
 
-
+ // console.log(cartsItems);
 
     const itemList = await cartsItems.map(item=>{
         const newItem = {
@@ -24,19 +25,56 @@ const newPayment = async ( cartsItems) =>{
         return newItem
     })
 
-    console.log(itemList);
-    console.log(itemList[0].price_data.product_data);
+    let emailData=[];
+
+    try {
+      await Promise.all(cartsItems.map(async elementCart => {
+
+        // console.log('estoy dentro del forEach');
+   
+         let findedService = await Service.findByPk(elementCart.id);
+   
+        // console.log('Pase el findedService');
+   
+         let getUser = await findedService.getUser();
+   
+        // console.log('Pase el getUser');
+   
+         let emailDataObjetc = {
+           nameService : findedService.nameService,
+           emailService: getUser.email
+         }
+   
+        // console.log('este es el objeto de correos: ',emailDataObjetc);
+   
+         emailData=[... emailData, emailDataObjetc];
+   
+        //  console.log('vamos a ver que le pasa a emailData...', emailData);
+   
+       }))
+
+       const metadata = emailData.reduce((result, { nameService, emailService }, index) => {
+        result[`nameService_${index}`] = nameService;
+        result[`emailService_${index}`] = emailService;
+        return result;
+      }, {});
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items:itemList,
+        metadata:metadata,
         mode: 'payment',
         success_url: 'http://localhost:3001/payment/success',
         cancel_url: 'http://localhost:3001/payment/cancel'
     })
-    
+
+    console.log('Aqui deberia estar la data: ',emailData);
 
     return session;
+
+  } catch (error) {
+    console.log(error);
+  }
 
 }
 
