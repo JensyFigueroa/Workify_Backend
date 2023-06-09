79d975a5d5814425e2a19de2309e884b8fc10fbd
Danieltm95 <docampoc95@gmail.com>
2023-06-08 20:03:57 -0500

    force false

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { conn } = require("./src/db.js");
 const server = require("./src/app");
 
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   console.log("connect to DB");
   server.listen(process.env.PORT, () => {
     console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
