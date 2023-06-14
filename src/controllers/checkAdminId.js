5e3125c2c0fa38c13b8a26f3b7d2c24f0b6db06a
Danieltm95 <docampoc95@gmail.com>
2023-06-14 18:36:20 -0500

    added new admins

--- src/controllers/checkAdminId.js
+++ src/controllers/checkAdminId.js
@@ -3,7 +3,7 @@ const { Service, User } = require('../db.js');
 const checkAdminId = async (body) => {
   const { id } = body;
 //console.log(id, "id de la personaque se logeo");
-if( id === "7rcbhZc7hvRk42ZbFGC5txDynVG3" || id === "CQsoi1akLESx1VaBrKsDRkVhAiE3" || id === "2F0nWmNmtuOTfpgy7672EZcoSll2" ){
+if( id === "DU3MrZyotOWdhxy2XcyLKftNhiL2" ){
     return {
     bolean: true,
      url: "https://workify-admin-dashboard.vercel.app/"
@@ -16,4 +16,4 @@ if( id === "7rcbhZc7hvRk42ZbFGC5txDynVG3" || id === "CQsoi1akLESx1VaBrKsDRkVhAiE
 module.exports = { checkAdminId };
 
 
-//2F0nWmNmtuOTfpgy7672EZcoSll2
+//2F0nWmNmtuOTfpgy7672EZcoSll2 .....  || id === "7rcbhZc7hvRk42ZbFGC5txDynVG3" || id === "2F0nWmNmtuOTfpgy7672EZcoSll2"
